0348b411b98e716138042c8425c6c67b5e28f171
swapnil-sachan <swapnilsachan03@gmail.com>
2023-06-08 05:02:36 +0530

    initial commit - till property image uploads

---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import React from 'react'
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+import { BiRupee } from 'react-icons/bi';
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  disabled,
+  formatPrice,
+  required,
+  register,
+  errors
+}) => {
+  return (
+    <div className='w-full relative'>
+      { formatPrice && (
+        <BiRupee
+          size={24}
+          className='
+            text-neutral-500
+            absolute
+            top-5
+            left-2
+          '
+        />
+      )}
+
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=' '
+        type={type}
+        className={`
+          peer
+          w-full
+          p-3
+          pt-6
+          font-light
+          bg-white
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? `pl-9` : `pl-4`}
+          ${errors[id] ? `border-rose-500` : `border-neutral-300`}
+          ${errors[id] ? `focus:border-rose-500` : `focus:border-black`}
+        `}
+      />
+
+      <label
+        className={`
+          absolute
+          text-md
+          duration-500
+          transform
+          -translate-y-3
+          scale-75
+          top-5
+          z-10
+          origin-[0]
+          ${formatPrice ? `left-9` : `left-4`}
+          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0
+          peer-focus:scale-75
+          peer-focus:-translate-y-3
+          ${errors[id] ? `text-rose-500` : `text-zinc-400`}
+        `}
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default Input
---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,154 @@
+'use client';
+
+import { signIn } from "next-auth/react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
+import { useRouter } from "next/navigation";
+
+const LoginModal = () => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+    }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    signIn('credentials', {
+      ...data,
+      redirect: false,
+    })
+    .then((res) => {
+      setIsLoading(false);
+      
+      if(res?.ok) {
+        toast.success('Logged in successfully!');
+        router.refresh();
+        loginModal.onClose();
+      }
+
+      if(res?.error) {
+        toast.error(res.error);
+      }
+    })
+  }
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading
+        title="Welcome back"
+        subtitle="Login to your account!"
+      />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr/>
+      
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn('google')}
+      />
+
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
+
+      <div
+        className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+        "
+      >
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div>
+            First time using Airbnb?
+          </div>
+
+          <div
+            onClick={() => {
+              loginModal.onClose();
+              registerModal.onOpen();
+            }}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            "
+          >
+            Sign up
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  )
+}
+
+export default LoginModal
